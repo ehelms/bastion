d90eef6c6e4a849de66477a0c0086aca8e7ca481
Eric D. Helms <ericdhelms@gmail.com>
2015-12-09 06:45:20 -0500

    Bookmark

--- app/assets/javascripts/bastion/components/bst-bookmark.directive.js
+++ app/assets/javascripts/bastion/components/bst-bookmark.directive.js
@@ -4,37 +4,43 @@
 * @description
 * Provides the bookmarked items for a dropdown menu
 */
-angular.module('Bastion.components').directive('bstBookmark', ['$http', function ($http) {
+angular.module('Bastion.components').directive('bstBookmark', function () {
     return {
         scope: {
-            controllerName: '='
+            controllerName: '=',
+            query: '='
         },
         templateUrl: 'components/views/bst-bookmark.html',
-        link: function (scope) {
+        controller: ['$scope', '$http', function ($scope, $http) {
+            $scope.newBookmark = {};
+
             $http({
                 method: 'GET',
                 url: '/api/v2/bookmarks',
-                params: {search: 'controller=' + scope.controllerName}
+                params: {search: 'controller=' + $scope.controllerName}
             }).then(function successCallback(response) {
-                scope.bookmarks = response.data.results;
+                $scope.bookmarks = response.data.results;
             }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
             });
-            scope.add = function () {
-                scope.openModal();
-            }
-            scope.save = function () {
+
+            $scope.add = function () {
+                $scope.newBookmark.query = $scope.query;
+                $scope.openModal();
+            };
+
+            $scope.save = function () {
                 $http({
                     method: 'POST',
                     url: '/api/v2/bookmarks',
-                    data: {controller: scope.controllerName}
+                    data: {controller: $scope.controllerName}
                 }).then(function successCallback(response) {
 
                 }, function errorCallback(response) {
 
                 });
             }
-        }
+        }]
     };
-}]);
+});
